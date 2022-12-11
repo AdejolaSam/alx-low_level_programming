7769ed6f9f6a7f632e18bddaeba912bd11e24f6f
AdejolaSam <adejolajimmy@gmail.com>
2022-12-11 05:47:42 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main - prints a given phrase
- * Return:0 if exited properly,non-zero otherwise
+ * Return: 0 if exited properly,non-zero otherwise
  */
 
 int main(void)
 {      
-       puts("\"Programming is like a building multilingual puzzle");
+       puts("\"Programming is like building a multilingual puzzle");
        return (0);
 }
